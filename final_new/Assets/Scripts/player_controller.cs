3c731989cea21a35e02d9a2dfb6ad4bea958f16f
zhaseqq <119091013+zhaseqq@users.noreply.github.com>
2026-05-07 17:10:44 +0500

    checkpoint2

--- final_new/Assets/Scripts/player_controller.cs
+++ final_new/Assets/Scripts/player_controller.cs
@@ -1,34 +1,55 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
-public class player_controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
-
+    
     private Rigidbody rb;
+
+    
     private float movementX;
     private float movementY;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public float speed = 0;
+
+   
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
     }
 
-
+    
     void OnMove(InputValue movementValue)
     {
+        
         Vector2 movementVector = movementValue.Get<Vector2>();
 
+       
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    private void FixedUpdate()
+    
+    void FixedUpdate()
     {
+        
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
+       
         rb.AddForce(movement * speed);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
 }
